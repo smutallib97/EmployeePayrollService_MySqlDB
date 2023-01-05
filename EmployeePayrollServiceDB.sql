35e4ee0f47b1cbfbf2ab9bf458cdaf971c8c612e
smutallib97 <smutallib97@gmail.com>
2023-01-05 10:35:16 +0530

    Added: Ability to retrieve all the employee payroll data that is added to payroll service database

--- EmployeePayrollServiceDB.sql
+++ EmployeePayrollServiceDB.sql
@@ -8,3 +8,6 @@ salary Double NOT NULL, start DATE NOT NULL, PRIMARY KEY (id));
 Select * from payroll_service.employee_payroll;
 #UC3- Ability to create employee payroll data in the payroll service database as part of CURD Operation
 insert into employee_payroll (name, salary, start) values ( 'bill',100000.00,'2018-01-03'),('terisa',200000.00,'2019-11-13');
+
+#UC4- Ability to retrieve all the employee payroll data that is added to payroll service database
+select * from employee_payroll;
