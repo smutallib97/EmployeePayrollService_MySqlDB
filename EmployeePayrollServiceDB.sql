8a690169c79ede51ac7fb201228635ded67318a8
smutallib97 <smutallib97@gmail.com>
2023-01-06 09:04:58 +0530

    Added: Ability to extend employee_payroll data to store employee information like employee phone, address, department

--- EmployeePayrollServiceDB.sql
+++ EmployeePayrollServiceDB.sql
@@ -30,3 +30,9 @@ select avg(salary) from payroll_service.employee_payroll;
 select min(salary) from payroll_service.employee_payroll;
 select max(salary) from payroll_service.employee_payroll;
 select count(salary) from payroll_service.employee_payroll;
+
+#UC8
+alter table payroll_service.employee_payroll add phone_number varchar(25) after name;
+alter table payroll_service.employee_payroll add address varchar(100) DEFAULT 'TBD' after phone_number;
+alter table payroll_service.employee_payroll add department varchar(150) not null after address;
+
